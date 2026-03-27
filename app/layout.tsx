d45af50d97c5b955404eb0570b8ed52b2e119240
Godwynski <kuyagodwyn@gmail.com>
2026-03-27 10:11:59 +0800

    feat: Implement base application layout with Vercel Speed Insights and update ESLint dependencies.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata, Viewport } from "next";
 import { Manrope } from "next/font/google";
 import { Toaster } from "sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
+
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -40,6 +42,7 @@ export default function RootLayout({
       <body className={`${appSans.className} antialiased`}>
         {children}
         <Toaster position="bottom-right" richColors />
+        <SpeedInsights />
       </body>
     </html>
   );
